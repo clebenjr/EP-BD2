e447de5acd9fc5a84f1719b47c075064c6ce6481
Joao Pedro Marcondes <joaopopm@usp.br>
2025-05-20 14:58:14 -0300

    Adicionei a parte de conexao com o banco e o app flask

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,1 +1,29 @@
-print("Funcionando!")
+from flask import Flask, jsonify
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Carrega variáveis do arquivo .env
+load_dotenv()
+
+# Inicializa o app Flask
+app = Flask(__name__)
+
+# Conecta ao banco PostgreSQL usando as variáveis do .env
+""" conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
+) """
+
+@app.route('/')
+def index():
+    #cur = conn.cursor()
+    #cur.execute('SELECT * FROM sua_tabela LIMIT 10;')  # Substitua por sua tabela real
+    #rows = cur.fetchall()
+    #cur.close()
+    return "<h1>oi</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
