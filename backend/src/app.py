b58ca7c54099b6ec0c28b3263da15745a7d0bbc7
Joao Pedro Marcondes <joaopopm@usp.br>
2025-06-03 16:03:02 -0300

    Cadastrar está funcionando

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,11 +7,18 @@ FRONTEND_DIR = Path(__file__).resolve().parents[2] / 'frontend' / 'src'
 app = Flask(__name__, template_folder=FRONTEND_DIR, static_folder=FRONTEND_DIR / 'static')
 
 @app.route('/')
+@app.route('/index')
 def index():
     bd = Banco()
     print(bd.busca_tabela())
     bd.close()
     return render_template('index.html')
+
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template('cadastrar.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
