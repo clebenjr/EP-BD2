93b1b42f3efce9fed98f7097b8541e62bd7fe814
Joao Pedro Marcondes <joaopopm@usp.br>
2025-06-06 17:54:46 -0300

    Adiciona funcionalidade para buscar e exibir divisões no formulário de cadastro de chefes

--- backend/src/app.py
+++ backend/src/app.py
@@ -56,8 +56,8 @@ def cadastrar_chefe():
         pass
     # Renderiza o template para cadastrar um conflito
     else:
-        # Aqui você pode carregar dados necessários para o formulário, se necessário
-        return render_template('cadastrar-chefes.html')
+        nomes_divisoes = bd.buscar_nomes_divisoes()
+        return render_template('cadastrar-chefes.html', nomes_divisoes=nomes_divisoes)
 
 
 @app.route('/cadastrar/grupos', methods=['GET', 'POST'])
