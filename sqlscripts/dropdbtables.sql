0b7a8efa695901c35e89a3ee199ad2b409443851
clebenjr <clebenjuniorcgarcia@hotmail.com>
2025-06-24 22:30:08 -0300

    atualizando dropdbtables.sql

--- sqlscripts/dropdbtables.sql
+++ sqlscripts/dropdbtables.sql
@@ -1,5 +1,5 @@
 
-DROP TABLE IF EXISTS 
+DROP TABLE IF EXISTS
     ep2_bd2.afeta,
     ep2_bd2.arma,
     ep2_bd2.chefe_militar,
@@ -7,18 +7,17 @@ DROP TABLE IF EXISTS
     ep2_bd2.depende_organizacao,
     ep2_bd2.dialoga,
     ep2_bd2.divisao,
-    ep2_bd2.etnias_conflito,          -- Nome atualizado
-    ep2_bd2.fornece_arma_grupo,       -- Nome atualizado
+    ep2_bd2.etnias_conflito,
+    ep2_bd2.fornece_arma_grupo,
     ep2_bd2.grupo_armado,
     ep2_bd2.lider_politico,
-    ep2_bd2.materias_primas_conflito, -- Nome atualizado
+    ep2_bd2.materias_primas_conflito,
     ep2_bd2.organizacao_mediadora,
     ep2_bd2.pais,
     ep2_bd2.participa_grupo,
     ep2_bd2.participa_organizacao,
-    ep2_bd2.possui_arma_traficante,   -- Nome atualizado
-    ep2_bd2.regioes_conflito,         -- Nome atualizado
-    ep2_bd2.religioes_conflito,       -- Nome atualizado
+    ep2_bd2.possui_arma_traficante,
+    ep2_bd2.regioes_conflito,
+    ep2_bd2.religioes_conflito,
     ep2_bd2.traficante
 CASCADE;
-
